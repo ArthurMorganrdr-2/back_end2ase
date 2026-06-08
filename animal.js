92ae44d40bbecd59f3c67d633de0956894b53b61
arthur <gabrielle.alves.silva@escola.pr.gov.br>
2026-06-08 13:52:34 +0000

    v1

--- animal.js
+++ animal.js
@@ -6,4 +6,12 @@ const animal = {
     vacinado: true
 };
 
+//console.log(animal);
 console.log(animal.nome);
+console.log(animal.tutor);
+
+animal.idade = 6;
+
+//console.log(animal);
+
+console.log(`${animal.nome} é um ${animal.especie} com a idade ${animal.idade} e possui sua vacina em dia ${animal.vacinado}`);
